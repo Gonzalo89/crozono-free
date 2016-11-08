50239ad1d2be02c704c8af440c257a7077bba630
Gonzalo <gonzalo89@ymail.com>
2016-11-07 22:35:13 -0300

    Fix path

--- src/tools/wash.py
+++ src/tools/wash.py
@@ -9,7 +9,7 @@
 """
 import os
 import sys
-sys.path.append("/usr/lib/python2.7/dist-packages")
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
 import pexpect
 import src.settings as settings
 
